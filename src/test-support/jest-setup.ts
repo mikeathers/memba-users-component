26a7473cb003c58c4e8da434a39bf9cdeaa29cb1
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-10 17:51:45 +0100

    feat: adds frontend url to jest env

--- src/test-support/jest-setup.ts
+++ src/test-support/jest-setup.ts
@@ -1,4 +1,8 @@
+import CONFIG from '../config'
+
 const error = console.error
+
+process.env.FRONTEND_BASE_URL = CONFIG.FRONTEND_BASE_URL_DEV
 
 const throwError = function (message: string | Error, ...args: unknown[]) {
   error.apply(console, args) // keep default behaviour
