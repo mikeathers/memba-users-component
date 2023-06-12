d8e4feb91fd7c82c2971db57f37abfada3bbb2bb
Mike Atherton <athers_05@hotmail.co.uk>
2023-06-12 08:32:08 +0100

    feat: adds more loggins for create group 2

--- src/handlers/user-admin/functions/create-user-group.ts
+++ src/handlers/user-admin/functions/create-user-group.ts
@@ -8,21 +8,25 @@ interface createUserGroupProps {
 }
 
 export const createUserGroup = (props: createUserGroupProps) => {
-  const {cognito, tenantName, userPoolId, userGroupRoleArn} = props
+  try {
+    const {cognito, tenantName, userPoolId, userGroupRoleArn} = props
 
-  const params = {
-    GroupName: `${tenantName}UsersGroup`,
-    UserPoolId: userPoolId,
-    Description: `A group for ${tenantName} users`,
-    Precedence: 4,
-    RoleArn: userGroupRoleArn,
+    const params = {
+      GroupName: `${tenantName}UsersGroup`,
+      UserPoolId: userPoolId,
+      Description: `A group for ${tenantName} users`,
+      Precedence: 4,
+      RoleArn: userGroupRoleArn,
+    }
+
+    console.log('Create Group Params: ', params)
+
+    cognito.createGroup(params, (err, data) => {
+      console.log('CALLBACK')
+      console.log('CREATE GROUP ERR: ', err)
+      console.log('CREATE GROUP RESULT:', data)
+    })
+  } catch (err) {
+    console.error('ERROR', err)
   }
-
-  console.log('Create Group Params: ', params)
-
-  cognito.createGroup(params, (err, data) => {
-    console.log('CALLBACK')
-    console.log('CREATE GROUP ERR: ', err)
-    console.log('CREATE GROUP RESULT:', data)
-  })
 }
